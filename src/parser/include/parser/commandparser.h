983c47fb7ada1306eaa69d2c3c92be211a31d1dc
Nerdhaus-Playing <Nerdhaus.playing@gmail.com>
2020-03-01 20:55:26 +0100

    Finished initial version of commandline parser

--- src/parser/include/parser/commandparser.h
+++ src/parser/include/parser/commandparser.h
@@ -1,12 +1,30 @@
 #pragma once
 
 #include <string>
+#include <map>
 
 namespace nhp::battleplan::parser
 {
+	class CommandAction
+	{
+	public:
+		virtual bool handleSubcommand(const std::string& subcommand) = 0;
+		virtual bool handleEmptyCall() = 0;
+		virtual std::string getDescription() const = 0;
+		virtual std::string getUsage() const = 0;
+	};
+
 	class CommandParser
 	{
-
+	public:
 		bool parse(const std::string& command);
+		void setCommandAction(const std::string& command, CommandAction* action);
+	private:
+		void printHelpLine(const std::string& command, CommandAction* action);
+
+		std::map<std::string, CommandAction*> m_actionDictionary;
+		size_t m_maxCommandLength;
+		size_t m_maxDescriptionLength;
+		size_t m_maxUsageLength;
 	};
 }
--- src/parser/src/commandparser.cpp
+++ src/parser/src/commandparser.cpp
@@ -1,8 +1,77 @@
 #include "parser/commandparser.h"
+#include <exception>
+#include <stdexcept>
+#include <iostream>
 
 using namespace nhp::battleplan::parser;
 
+
 bool CommandParser::parse(const std::string& command)
 {
-	return true;
+	for (const auto& action : m_actionDictionary)
+	{
+		const auto commandLength = action.first.size();
+		if (command.size() < commandLength)
+		{
+			continue;
+		}
+		if (command.size() == commandLength && command == action.first)
+		{
+			return action.second->handleEmptyCall();
+		}
+		if (command.substr(0, commandLength + 1) == action.first + " ")
+		{
+			return action.second->handleSubcommand(command.substr(commandLength + 1));
+		}
+	}
+	if (command == "help")
+	{
+		for (const auto& action : m_actionDictionary)
+		{
+			printHelpLine(action.first, action.second);
+		}
+	}
+	throw std::runtime_error("unknown command");
+}
+
+void CommandParser::setCommandAction(const std::string& command, CommandAction* action)
+{
+	if (m_actionDictionary.find(command) != m_actionDictionary.end())
+	{
+		m_actionDictionary[command] = action;
+		if (m_maxCommandLength < command.size())
+		{
+			m_maxCommandLength = command.size();
+		}
+		if (m_maxDescriptionLength < action->getDescription().size())
+		{
+			m_maxDescriptionLength = action->getDescription().size();
+		}
+		if (m_maxUsageLength < action->getUsage().size())
+		{
+			m_maxUsageLength = action->getUsage().size();
+		}
+	}
+	else
+	{
+		throw std::runtime_error("command already installed");
+	}
+}
+
+void printStringWithForcedLength(size_t length, const std::string& text)
+{
+	std::cout << text;
+	int i = text.size();
+	for (int i = text.size(); i < length + 1; i++)
+	{
+		std::cout << " ";
+	}
+}
+
+void CommandParser::printHelpLine(const std::string& command, CommandAction* action)
+{
+	printStringWithForcedLength(m_maxCommandLength, command);
+	printStringWithForcedLength(m_maxUsageLength, action->getUsage());
+	printStringWithForcedLength(m_maxDescriptionLength, action->getDescription());
+	std::cout << std::endl;
 }
