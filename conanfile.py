197d0ba25921d65193948249a7c3819a87920b73
Nerdhaus-Playing <Nerdhaus.playing@gmail.com>
2020-03-04 20:56:36 +0100

    Added sfml Library

--- conanfile.py
+++ conanfile.py
@@ -28,14 +28,17 @@ class ArbitraryCommandLineToolConan(ConanFile):
    default_options = {"shared": False, "with_tests": False, "with_demo": False}
    generators = "cmake"
 
+   def requirements(self):
+      self.requires("sfml/2.5.1@bincrafters/stable")
+      self.options["sfml"].shared = True
+      self.options["sfml"].graphics = True
+      self.options["sfml"].window = True
+      self.requires("bzip2/1.0.8@conan/stable")
+
    def build_requirements(self):
       if self.options.with_tests == True :
          self.build_requires("gtest/1.10.0@nerdhaus-mirror/stable")
       self.build_requires("boost/1.71.0@conan/stable")
-      self.build_requires( "sfml/2.5.1@bincrafters/stable")
-      self.options["sfml"].shared = True
-      self.options["sfml"].graphics = True
-      self.options["sfml"].window = True
 
    def build(self):
       cmake = CMake(self)
@@ -43,6 +46,14 @@ class ArbitraryCommandLineToolConan(ConanFile):
       cmake.definitions["BUILD_WITH_DEMO"] = self.options.with_demo
       cmake.configure()
       cmake.build()
+
+   def imports(self):
+      if(self.settings.os=="Windows"):
+         self.copy("*.dll", dst="../bin/"+str(self.settings.build_type), src="bin")
+         self.copy("*.dylib*", dst="../bin/"+str(self.settings.build_type), src="lib")
+      else:
+         self.copy("*.dll", dst="../bin", src="bin")
+         self.copy("*.dylib*", dst="../bin", src="lib")
 
    def package(self):
       cmake = CMake(self)
